8cee0d453085d6a6a840dc161ade53f611fb6401
thabsheer <thabsheer@mobme.in>
2011-12-13 15:19:59 +0530

    made the page more dynamic

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,16 +4,22 @@ describe PagesController do
 
   render_views
 
+  before(:each) do
+    #
+    # Define @base_title here.
+    #
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-
+    
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => @base_title + " | Home")
     end
   end
 
@@ -26,8 +32,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => @base_title + " | Contact")
     end
   end
 
@@ -40,8 +45,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
+                                    :content => @base_title + " | About")
     end
   end
 
